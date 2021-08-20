5aeb3dd561048d9410ebbbb172ef285a04395b4a
Pedro Sanchez <74252597+PedroValenciano@users.noreply.github.com>
2021-08-20 16:22:24 -0300

    Update miscript.js

--- miscript.js
+++ miscript.js
@@ -1,11 +1,19 @@
 (() => {
+    //first creat a variable bringing the button from html
     let cambioDeColor = document.getElementById('btnCambio');
+    /*here i create an a click event and define which actions going to happens
+    every time i click the button*/
     cambioDeColor.addEventListener('click', ()=>{
+        /*function to generate HEX color*/
         function generarCodigoHex(longitud){
+            /*first i declare 2 variables with empty string*/
             let codigoHex = "";
             let codigoHex2 = "";
+            /*then i declare the argument "longitud" length*/
             longitud = 6;
+            //Hex color are define by numbers from 0-9 and letter from a-f, i declare a string with this parameters
             let elementosCodigoHex = "abcdef0123456789";
+            //with a for loop i declare the conditions
             for (let i =0; i<longitud; i++){
                 codigoHex+= elementosCodigoHex.charAt(Math.floor(Math.random()*elementosCodigoHex.length));
                 codigoHex2+= elementosCodigoHex.charAt(Math.floor(Math.random()*elementosCodigoHex.length));
